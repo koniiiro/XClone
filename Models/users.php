34d170e0b3b9c8ffdba79707058261fbd921222f
koniiiro <ikuekoni@gmail.com>
2026-01-10 18:13:45 +0900

    ユーザーモデルを作成

--- Models/users.php
+++ Models/users.php
@@ -2,3 +2,45 @@
 /////////////////////////
 //ユーザーデータを処理
 ///////////////////////////
+
+/**
+ * ユーザーを作成
+ *
+ * @param array $data
+ * @return bool
+ */
+function createUser(array $data){
+    //DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    //DB接続    
+    if($mysqli->connect_errno){
+        echo 'MYSQLの接続に失敗しました。: ' . $mysqli->connect_error . "\n";
+        exit;
+    }
+    // SQLクエリを作成(ユーザー新規登録)
+    $query = 'INSERT INTO users(email, name, nickname, password) VALUES(?, ?, ?, ?)';
+
+    //パスワードをハッシュ値に変換
+    $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+    //プリペアドステートメントに、作成したクエリを登録
+    $statement = $mysqli ->prepare($query);
+
+    //クエリをプレースホルダ(？)にカラムの値を紐づけ
+    $statement->bind_param('ssss', $data['email'], $data['name'],$data['nickname'],$data['password']);
+    
+    // クエリを実行
+    $response = $statement->execute();
+
+    //エラーの場合は、エラー表示
+    if($response === false){
+        echo 'エラーメッセージ :' . $mysqli->error . "\n";
+    }
+
+    //DB接続を開放
+    $statement->close();
+    $mysqli->close();
+
+    //結果を返却
+    return $response;
+}
--- config.php
+++ config.php
@@ -6,3 +6,8 @@ ini_set('display_errors',1);
 date_default_timezone_set('Asia/Tokyo');
 //URLディレクトリ設定
 define('HOME_URL','/XClone/');
+//データベースの接続情報
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'x_clone');
