31fe815d672b4c754834b38b89c9f3fe2102209c
koniiiro <ikuekoni@gmail.com>
2026-01-16 12:10:17 +0900

    いいね！のコントローラーを作成

---
+++ Controllers/like.php
@@ -0,0 +1,52 @@
+<?php
+
+/////////////////////////
+//ライクコントローラー
+///////////////////////////
+
+//設定を読み込み
+include_once'../config.php';
+// 便利な関数を読み込む
+include_once '../util.php';
+
+//いいね！データ操作モデルを読み込み
+include_once '../Models/likes.php';
+
+
+//ログインチェック
+$user = getUserSession();
+if(!$user){
+    //404エラー
+    header('HTTP/1.0 404 Not Found');
+    exit;
+}
+
+//いいね！する
+$like_id = null;
+if(isset($_POST['tweet_id'])){
+    $data = [
+        'tweet_id'=>$_POST['tweet_id'],
+        'user_id'=>$user['id'],
+    ];
+}
+//いいね！登録
+createLike($data);
+
+//いいね！を削除する
+if(isset($_POST['like_id'])){
+    $data=[
+    'like_id'=>$_POST['like_id'],
+    'user_id'=>$user['id'],
+    ];
+    //いいね！を削除
+    deleteLike($data);
+
+}
+//JSON形式で結果を返却
+$response= [
+    'message'=>'successful',
+    //いいね！したときには値が入ります
+    'like_id'=>$like_id,
+];
+header('Content-Type: application/json; charset=uft-8');
+echo json_sncode($response);
