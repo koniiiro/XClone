cb6a6219a9651fb1e4b739445a0ba58930caf418
koniiiro <ikuekoni@gmail.com>
2026-01-22 18:15:05 +0900

    ノーティフィケーションモデルに通知一覧の関数を作成

--- Models/notifications.php
+++ Models/notifications.php
@@ -44,3 +44,51 @@ function createNotification(array $data)
     return $response;
 }
 
+/**
+ * 通知の一覧を取得
+ *@param int $user_id
+ * @return array|false
+ */
+function findNotifications(int $user_id){
+
+    //DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    //接続エラーの場合    
+    if($mysqli->connect_errno){
+        echo 'MYSQLの接続に失敗しました。: ' . $mysqli->connect_error . "\n";
+        exit;
+    }
+
+    //エスケープ
+    $user_id = $mysqli->real_escape_string($user_id);
+
+    //SQLクエリを作成
+    $query = <<<SQL
+        SELECT
+            --通知
+            N.id AS notification_id,
+            N.message as notification_message,
+            --ユーザー
+            U.name AS user_name,
+            U.nickname AS user_nickname,
+            U.image_name AS user_image_name 
+        FROM notifications AS N 
+        WHERE N.status = 'active' AND N.received_user_id ='$user_id'
+        JOIN users AS U
+            ON U.id = N.sent_user_id AND U.status = 'active'
+        ORDER BY N.created_at DESC
+        LIMIT 50
+    SQL;
+
+    //クエリを実行
+    if($result = $mysqli->query($query)) {
+        //全件取得
+        $notifications = $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        $notifications = false;
+        echo 'エラーメッセージ: ' . $mysqli->error . "\n";
+    }
+    $mysqli->close();
+    return $notifications;
+}
